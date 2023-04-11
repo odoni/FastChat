0bcd32cc1b459ea68d85c3c5a831f898690b8608
felipe.odoni <felipe.odoni@gmail.com>
2023-04-11 22:55:29 +0100

    fix

--- fastchat/serve/inference.py
+++ fastchat/serve/inference.py
@@ -69,11 +69,13 @@ def generate_stream(model, tokenizer, params, device,
 
     for i in range(max_new_tokens):
         if i == 0:
+            print("ok0")
             out = model(
                 torch.as_tensor([input_ids], device=device), use_cache=True)
             logits = out.logits
             past_key_values = out.past_key_values
         else:
+            print("ok1")
             attention_mask = torch.ones(
                 1, past_key_values[0][0].shape[-2] + 1, device=device)
             out = model(input_ids=torch.as_tensor([[token]], device=device),
