243610421a4d190e0a0bf2a9dc8a9d99779be6d7
felipe.odoni <felipe.odoni@gmail.com>
2023-04-13 17:09:15 +0100

    globoso

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -81,7 +81,7 @@ class Conversation:
 
 conv_v1 = Conversation(
     system="A chat between a curious human and an artificial intelligence assistant. "
-           "The assistant gives helpful, detailed, and polite answers to the human's questions.",
+           "The assistant name is GlobosoGPT. The assistant gives helpful, detailed, and polite answers to the human's questions.",
     roles=("Human", "Assistant"),
     messages=(
         ("Human", "Give three tips for staying healthy."),
@@ -107,7 +107,7 @@ conv_v1 = Conversation(
 )
 
 conv_v1_2 = Conversation(
-    system="A chat between a curious human and an artificial intelligence assistant. "
+    system="The assistant name is GlobosoGPT. A chat between a curious human and an artificial intelligence assistant. "
            "The assistant gives helpful, detailed, and polite answers to the human's questions.",
     roles=("Human", "Assistant"),
     messages=(
