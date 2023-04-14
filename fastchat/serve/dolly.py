723e19b0411b421ee39777513b94d161a84a0e5b
felipe.odoni <felipe.odoni@gmail.com>
2023-04-14 16:15:09 +0100

    dolly

--- fastchat/serve/dolly.py
+++ fastchat/serve/dolly.py
@@ -3,9 +3,11 @@ from transformers import pipeline
 from .instruct_pipeline import InstructionTextGenerationPipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+print("Loading model")
 tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-7b", padding_side="left", torch_dtype=torch.bfloat16)
 model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-7b", device_map="auto", torch_dtype=torch.bfloat16)
 
+print("Loading text generation")
 generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
 
-generate_text("Explain to me the difference between nuclear fission and fusion.")
+print(generate_text("Explain to me the difference between nuclear fission and fusion."))
