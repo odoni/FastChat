fdd5dc7e3b667e329e8c602021a9f10a22ac39af
felipe.odoni <felipe.odoni@gmail.com>
2023-04-16 08:55:14 +0100

    dolly

--- fastchat/serve/dolly.py
+++ fastchat/serve/dolly.py
@@ -7,7 +7,7 @@ from peft import PeftModel
 print("Loading model")
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-12b", padding_side="left", torch_dtype=torch.bfloat16)
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-12b", device_map="auto", torch_dtype=torch.bfloat16)
-model = PeftModel.from_pretrained(model, "/home/odoni/dolly/output/dolly2_local_v2", torch_dtype=torch.float16)
+model = PeftModel.from_pretrained(model, "/home/odoni/dolly/output/dolly2_local_v2", torch_dtype=torch.bfloat16)
 
 print("Loading text generation")
 generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
