f3ac5677b4883c265592c04c84e3949b41a4d5e8
felipe.odoni <felipe.odoni@gmail.com>
2023-04-16 09:02:43 +0100

    dolly

--- fastchat/serve/dolly.py
+++ fastchat/serve/dolly.py
@@ -5,9 +5,9 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 
 print("Loading model")
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-12b", padding_side="left", torch_dtype=torch.bfloat16)
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-12b", device_map="auto", torch_dtype=torch.bfloat16)
-model = PeftModel.from_pretrained(model, "/home/odoni/dolly/output/dolly2_local_v2", torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-12b", padding_side="left")
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-12b", device_map="auto")
+model = PeftModel.from_pretrained(model, "/home/odoni/dolly/output/dolly2_local_v2")
 
 print("Loading text generation")
 generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
