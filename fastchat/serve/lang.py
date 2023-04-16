e39f9535c9c95c103a9cd26a7442a7cd732931ae
felipe.odoni <felipe.odoni@gmail.com>
2023-04-16 11:57:11 +0100

    lang

---
+++ fastchat/serve/lang.py
@@ -0,0 +1,30 @@
+from langchain.llms import HuggingFacePipeline, LLMChain, PromptTemplate
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, AutoModelForSeq2SeqLM
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+model_id = '~/vicuna/vicuna-7b-delta-v0'# go for a smaller model if you dont have the VRAM
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, load_in_8bit=True)
+
+pipe = pipeline(
+    "text-generation",
+    model=model, 
+    tokenizer=tokenizer, 
+    max_length=512
+)
+
+local_llm = HuggingFacePipeline(pipeline=pipe)
+
+llm_chain = LLMChain(prompt=prompt, 
+                     llm=local_llm
+                     )
+
+question = "What is the capital of England?"
+
+print(llm_chain.run(question))
