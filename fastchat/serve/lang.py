db7d95b2075daa0aea9d9cd531ab973d64269bc4
felipe.odoni <felipe.odoni@gmail.com>
2023-04-19 11:54:15 +0100

    lang

--- fastchat/serve/lang.py
+++ fastchat/serve/lang.py
@@ -62,4 +62,4 @@ async def post_message(message_obj: Message):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
