374047c7f38bcf72fe8fa4b18211cf9ffcc06f9b
felipe.odoni <felipe.odoni@gmail.com>
2023-04-26 13:59:48 +0100

    fix promtp

--- fastchat/serve/api.py
+++ fastchat/serve/api.py
@@ -27,8 +27,8 @@ def chat():
 def clean_prompt(prompt: str):
     new_prompt =  prompt.replace("Use the following portion of a long document to see if any of the text is relevant to answer the question. \r\n", "")
     new_prompt =  new_prompt.replace("Return any relevant text verbatim.", "")
-    new_prompt =  new_prompt.replace("#", "")
     new_prompt =  new_prompt.replace("# ", "")
+    new_prompt =  new_prompt.replace("#", "")
     new_prompt =  new_prompt.replace("**", "")
     new_prompt = "### input: " + new_prompt
     return new_prompt
